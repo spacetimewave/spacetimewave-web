a575c3d1bd0662a5d1bd38c25af8009e39059ad2
Javier Hernández Sánchez <javierhersan1999@gmail.com>
2024-05-05 00:45:35 +0200

    chore(linter): add react version to eslint react plugin

--- eslint.config.js
+++ eslint.config.js
@@ -4,12 +4,19 @@ import tseslint from 'typescript-eslint'
 import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js'
 import eslintConfigPrettier from 'eslint-config-prettier'
 
+/* ESLint React Plugin Configuration */
 pluginReactConfig.rules = {
 	...pluginReactConfig.rules,
 	'react/react-in-jsx-scope': 0,
 	'react/prop-types': 'off',
 }
+pluginReactConfig.settings = {
+	react: {
+		version: 'detect',
+	},
+}
 
+/* ESLint Configuration */
 export default [
 	{ languageOptions: { globals: globals.browser } },
 	pluginJs.configs.recommended,
