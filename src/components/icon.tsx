7fccd0e4fa7d289fc5fe990c6aa9c284536f2a51
Javier Hernández Sánchez <javierhersan1999@gmail.com>
2024-05-05 00:21:44 +0200

    feat(components): add spacetimewave icon component

---
+++ src/components/icon.tsx
@@ -0,0 +1,5 @@
+import icon from '../assets/icon-expanded.png' // replace with the actual path to your logo file
+
+const Icon = () => <img src={icon} height='40' width='40' alt='Logo' />
+
+export default Icon
