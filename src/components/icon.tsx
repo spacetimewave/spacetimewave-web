a381cd059d920420a3286c180da892808d11faa3
Javier Hernández Sánchez <javierhersan1999@gmail.com>
2024-05-21 01:11:36 +0200

    feat(icon): add icon component with spacetimewave logo

--- src/components/icon.tsx
+++ src/components/icon.tsx
@@ -1,5 +1,5 @@
 import icon from '../assets/icon-expanded.png' // replace with the actual path to your logo file
 
-const Icon = () => <img src={icon} height='40' width='40' alt='Logo' />
+const Icon = () => <img src={icon} height='30' width='30' alt='Logo' />
 
 export default Icon
