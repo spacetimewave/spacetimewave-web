634b5f1084d9497064e072716568fde2603a48e2
Javier Hernández Sánchez <javierhersan1999@gmail.com>
2024-06-05 20:57:39 +0200

    feat(services): add card to horizontal swiper and services list

--- src/components/horizontal-swiper.tsx
+++ src/components/horizontal-swiper.tsx
@@ -1,9 +1,56 @@
 import { Swiper, SwiperSlide } from 'swiper/react'
 import { Autoplay, Pagination, Navigation } from 'swiper/modules'
 
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from '@/components/ui/card'
+
 // Import Swiper styles
 import 'swiper/css'
 import 'swiper/css/pagination'
+
+const services = [
+	{
+		id: 1,
+		title: 'Software',
+		description: 'Ad hoc software',
+		content: 'Card Content',
+		footer: 'Card Footer',
+	},
+	{
+		id: 2,
+		title: 'AI',
+		description: 'Artificial Intelligence',
+		content: 'Consulting Content',
+		footer: 'Consulting Footer',
+	},
+	{
+		id: 3,
+		title: 'Blockchain',
+		description: '24/7 support services',
+		content: 'Support Content',
+		footer: 'Support Footer',
+	},
+	{
+		id: 4,
+		title: 'Data',
+		description: 'Big Data, Analytics, and Visualization',
+		content: '',
+		footer: '',
+	},
+	{
+		id: 5,
+		title: 'Infrastructure',
+		description: 'Azure Cloud Infrastucture',
+		content: '',
+		footer: '',
+	},
+]
 
 export default function HorizontalSwiper() {
 	return (
@@ -21,53 +68,26 @@ export default function HorizontalSwiper() {
 				}}
 				navigation={true}
 				loop={true}
-				className=''
+				className='pt-4 pb-6'
 			>
-				<SwiperSlide>
-					<div key={1} className=' bg-gray-200 p-4 rounded shadow-md h-80'>
-						Service 1
-					</div>
-				</SwiperSlide>
-				<SwiperSlide>
-					<div key={1} className=' bg-gray-200 p-4 rounded shadow-md h-80'>
-						Service 1
-					</div>
-				</SwiperSlide>
-				<SwiperSlide>
-					<div key={1} className=' bg-gray-200 p-4 rounded shadow-md h-80'>
-						Service 1
-					</div>
-				</SwiperSlide>
-				<SwiperSlide>
-					<div key={1} className=' bg-gray-200 p-4 rounded shadow-md h-80'>
-						Service 1
-					</div>
-				</SwiperSlide>
-				<SwiperSlide>
-					<div key={1} className=' bg-gray-200 p-4 rounded shadow-md h-80'>
-						Service 1
-					</div>
-				</SwiperSlide>
-				<SwiperSlide>
-					<div key={1} className=' bg-gray-200 p-4 rounded shadow-md h-80'>
-						Service 1
-					</div>
-				</SwiperSlide>
-				<SwiperSlide>
-					<div key={1} className=' bg-gray-200 p-4 rounded shadow-md h-80'>
-						Service 1
-					</div>
-				</SwiperSlide>
-				<SwiperSlide>
-					<div key={1} className=' bg-gray-200 p-4 rounded shadow-md h-80'>
-						Service 1
-					</div>
-				</SwiperSlide>
-				<SwiperSlide>
-					<div key={1} className=' bg-gray-200 p-4 rounded shadow-md h-80'>
-						Service 1
-					</div>
-				</SwiperSlide>
+				{services.map((service) => (
+					<SwiperSlide key={service.id}>
+						<div className='p-4'>
+							<Card className='bg-secondary min-h-[40vh]'>
+								<CardHeader>
+									<CardTitle>{service.title}</CardTitle>
+									<CardDescription>{service.description}</CardDescription>
+								</CardHeader>
+								<CardContent>
+									<p>{service.content}</p>
+								</CardContent>
+								<CardFooter>
+									<p>{service.footer}</p>
+								</CardFooter>
+							</Card>
+						</div>
+					</SwiperSlide>
+				))}
 			</Swiper>
 		</>
 	)
